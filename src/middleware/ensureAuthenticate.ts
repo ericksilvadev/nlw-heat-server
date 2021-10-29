7c52ec699ecc3a63ece9b921906872ae1b7aeab9
ericksilvadev <erick.silva6653@gmail.com>
2021-10-29 18:26:29 -0300

    heroku

--- src/middleware/ensureAuthenticate.ts
+++ src/middleware/ensureAuthenticate.ts
@@ -2,24 +2,28 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
 interface IPayload {
-  sub: string,
-};
+  sub: string;
+}
 
-export function ensureAuthenticate(request: Request, response: Response, next: NextFunction) {
+export function ensureAuthenticate(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
   const authToken = request.headers.authorization;
 
-  if(!authToken) {
+  if (!authToken) {
     return response.status(401).json({
       errorCode: 'token.invalid',
     });
-  };
+  }
 
-  const [,token] = authToken.split(' ');
+  const [, token] = authToken.split(' ');
   try {
     const { sub } = verify(token, process.env.JWT_SECRET || '') as IPayload;
     request.user_id = sub;
     return next();
-  } catch(err) {
-    return response.status(401).json({ errorCode: 'token.expired'});
+  } catch (err) {
+    return response.status(401).json({ errorCode: 'token.expired' });
   }
-};
+}
