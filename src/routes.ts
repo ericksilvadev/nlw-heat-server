08855bb337fd36d606482eb8fd1dc7a74f0ff2e8
ericksilvadev <erick.silva6653@gmail.com>
2021-10-29 17:43:15 -0300

    heroku

--- src/routes.ts
+++ src/routes.ts
@@ -7,19 +7,19 @@ import { ensureAuthenticate } from './middleware/ensureAuthenticate';
 
 const router = Router();
 
-router.get('/', (_req, res) => {
-  res.json({ connection: 'connected' });
+router.get('/health', (_req, res) => {
+  res.json('app is running');
 });
 
 router.post('/authenticate', new AuthenticateUserController().handle);
+
+router.get('/messages/last3', new GetLast3MessagesController().handle);
 
 router.post(
   '/messages',
   ensureAuthenticate,
   new CreateMessageController().handle
 );
-
-router.get('/messages/last3', new GetLast3MessagesController().handle);
 
 router.get('/profile', ensureAuthenticate, new ProfileUserController().handle);
 
